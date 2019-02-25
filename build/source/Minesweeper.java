c9098fcebff9fbd6e9f5b7669fddf8b769b58e48
cindelint <cindylee776@gmail.com>
2019-02-25 10:24:08 -0800

    added buttons

--- build/source/Minesweeper.java
+++ build/source/Minesweeper.java
@@ -1,14 +1,32 @@
+import processing.core.*; 
+import processing.data.*; 
+import processing.event.*; 
+import processing.opengl.*; 
 
-import de.bezier.guido.*;
+import de.bezier.guido.*; 
+
+import java.util.HashMap; 
+import java.util.ArrayList; 
+import java.io.File; 
+import java.io.BufferedReader; 
+import java.io.PrintWriter; 
+import java.io.InputStream; 
+import java.io.OutputStream; 
+import java.io.IOException; 
+
+public class Minesweeper extends PApplet {
+
+
+
 
 public static final int NUM_ROWS = 20;
 public static final int NUM_COLS = 20;
 private MSButton[][] buttons; //2d array of minesweeper buttons
 private ArrayList <MSButton> bombs; //ArrayList of just the minesweeper buttons that are mined
 
-void setup ()
+public void setup ()
 {
-    size(400, 400);
+    
     textAlign(CENTER,CENTER);
 
     // make the manager / starts GUIDO
@@ -112,4 +130,14 @@ public class MSButton {
         //your code here
         return numBombs;
     }
+}
+  public void settings() {  size(400, 400); }
+  static public void main(String[] passedArgs) {
+    String[] appletArgs = new String[] { "Minesweeper" };
+    if (passedArgs != null) {
+      PApplet.main(concat(appletArgs, passedArgs));
+    } else {
+      PApplet.main(appletArgs);
+    }
+  }
 }
